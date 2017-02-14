8fe1f3da58e65c31fc6b9578d05bfd64384f77b9
wangqifan <1694675518@qq.com>
2017-02-14 12:52:34 +0800

    1

---
+++ ProxyPool/Tool.cs
@@ -0,0 +1,33 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPool
+{
+     public  class Tool
+    {
+         public static bool IsAvailable(Proxy proxy)
+         {
+             bool result = false;
+             try
+             {
+                 WebProxy webproxy = new WebProxy(proxy.Adress, proxy.port);
+                 string html = HttpHelper.DownloadHtml("https://www.baidu.com/", webproxy);
+                 if (html.Contains("百度一下，你就知道"))
+                 {
+                     result = true;
+                 }
+             }
+             catch
+             {
+
+             }
+             return result;
+         }
+    }
+}
